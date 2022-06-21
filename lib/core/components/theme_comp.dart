d97f04a524318cbecc8698ae8c3ddf6ed100b365
Nodirbek Rasuljonov <nodirbekmaqsudjonovich@gmail.com>
2022-06-21 20:47:17 +0500

    some bugs fixed

--- lib/core/components/theme_comp.dart
+++ lib/core/components/theme_comp.dart
@@ -2,10 +2,10 @@ import 'package:exam/core/constants/color_const.dart';
 import 'package:flutter/material.dart';
 
 class ThemeComp {
-  static ThemeData lightmode = ThemeData.light();
+  static ThemeData lightmode = ThemeData();
   static ThemeData darkmode = ThemeData(
     colorScheme: ColorScheme.dark(
-      primary: ColorConst.darkmodeColor,
-    ),
+      primary: ColorConst.darkmodeColor
+    )
   );
 }
--- lib/core/constants/color_const.dart
+++ lib/core/constants/color_const.dart
@@ -2,6 +2,5 @@ import 'package:flutter/material.dart';
 
 class ColorConst{
   static Color whitetextColor=Colors.white;
-  static Color blackTextColor=Colors.black;
   static Color darkmodeColor=Color(0xff171725);
 }
